ca227f2405567e3a198f9ffdc9ed964d520cd752
Subramani V <106590803+subu-v@users.noreply.github.com>
2023-02-10 12:37:26 +0530

    Theme switcher done, hover effects on the svg done.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
-
+const themeSwitcher = document.querySelector(".app__toggle-slider");
+themeSwitcher.addEventListener("click", (e) => {
+  if (document.body.classList.contains("dark-theme")) {
+    document.body.classList.remove("dark-theme");
+  } else {
+    document.body.classList.add("dark-theme");
+  }
+});
